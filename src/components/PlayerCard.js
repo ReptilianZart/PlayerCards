6dff516eb292886e9669817ea8621c8bcb6a5023
ReptilianZart <andreasjjhome@gmail.com>
2022-11-13 15:38:56 +0100

    initial commit

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -13,37 +13,38 @@ function PlayerCard({ name, clan, country, playerData, playerStats }) {
 
     return (
         <div className="PlayerCard">
-            <div className="PlayerInfoLeft">
-                <p className="Name">{name}</p>
-                <p className="Clan">{clan}</p>
-            </div>
+
             <div className="ovrScore">
                 <p className="Score">{playerStats.ovr}</p>
                 <p className="Attribute">OVR</p>
             </div>
 
-            <div className="PlayerInfoRight">
-                <div className="ClanImage">
-                    <img src={clanImage.data && "data:".concat(clanImage.data.mime_type, ";base64,", clanImage.data.base64_image)}></img>
-                </div>   
-                <div className="CountryImage">
-                    <img src={countryImage.data && "data:image/webp;base64,".concat(countryImage.data.base64_image)}></img>
-                </div>
-                            
-            </div>
-
             <div className="CardBorder"></div>
-
-            <div className="CardDivider"></div>
 
             <div className="TopContainer">
                 <div className="PlayerAvatar">
                     <img src={playerImage.data && "data:image/webp;base64,".concat(playerImage.data.base64_image)}></img>
                 </div>
-            
+
             </div>
+            
+            <hr className="CardDivider"></hr>
 
             <div className="BottomContainer">
+                <div className="PlayerInfoLeft">
+                    <p className="Name">{name}</p>
+                    <p className="Clan">{clan}</p>
+                </div>
+
+                <div className="PlayerInfoRight">
+                    <div className="ClanImage">
+                        <img src={clanImage.data && "data:".concat(clanImage.data.mime_type, ";base64,", clanImage.data.base64_image)}></img>
+                    </div>
+                    <div className="CountryImage">
+                        <img src={countryImage.data && "data:image/webp;base64,".concat(countryImage.data.base64_image)}></img>
+                    </div>
+                </div>
+
                 <div className="Stats">
                     <div className="Coloumn">
 
